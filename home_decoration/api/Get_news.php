de7807aaefc3322b489003fb1b3400b58e73e960
Elvira <elvira.lysenko@yandex.ua>
2018-08-18 19:20:03 +0300

    new

---
+++ home_decoration/api/Get_news.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'dbconnect.php'; // подключаем скрипт
+// подключаемся к серверу
+$link = mysqli_connect($host, $user, $password, $database) 
+    or die("Ошибка " . mysqli_error($link));
+$link->set_charset("utf8");
+
+$query ="SELECT * FROM news";
+
+$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+if($result)
+{
+   $data = array(); // в этот массив запишем то, что выберем из базы
+    while($row = mysqli_fetch_assoc($result)){ // оформим каждую строку результата как ассоциативный массив
+    $data[] = $row; // допишем строку из выборки как новый элемент результирующего массива
+}
+    echo json_encode($data); // и отдаём как json
+     
+    // очищаем результат
+    mysqli_free_result($result);
+}
+// закрываем подключение
+mysqli_close($link); 
+
+?>
